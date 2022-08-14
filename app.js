28c28997eff5ab76ecfbce8cd531e90320328a74
Prasad Kodithuwakku <lahirumu98@gmail.com>
2022-08-14 10:29:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function getPassword(){
+    const chars= "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$%^&*()_+?><:{}[]'";
+
+    let passwordLength = document.getElementById('passwordLength').value;
+    let password='';
+
+    for(let i=0; i<passwordLength;i++){
+        let randomNumber = Math.floor(Math.random()*chars.length);
+        
+
+        password += chars.substring(randomNumber, randomNumber+1);
+    }
+    document.getElementById('password').value = password;
+}
